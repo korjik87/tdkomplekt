ef41b9b0e4afe05f16cbaf90c33742a517511239
Sergey <sprakapovich@buffergroup.com>
2024-02-08 09:01:53 +0300

    fix name attributes

--- laravel/resources/views/livewire/create-customer.blade.php
+++ laravel/resources/views/livewire/create-customer.blade.php
@@ -1,37 +1,35 @@
 <div>
     <form wire:submit.prevent="save">
-        <label for="title">Name:</label>
+        <label for="email">Name:</label>
         <input type="string" id="email" wire:model="name">
         <div>@error('name') {{ $message }} @enderror</div>
         <br>
-        <label for="title">surname:</label>
+        <label for="surname">surname:</label>
         <input type="string" id="surname" wire:model="surname">
         <div>@error('surname') {{ $message }} @enderror</div>
         <br>
-        <label for="title">Patronymic:</label>
+        <label for="patronymic">Patronymic:</label>
         <input type="string" id="patronymic" wire:model="patronymic">
         <div>@error('patronymic') {{ $message }} @enderror</div>
         <br>
-        <label for="title">Email:</label>
+        <label for="email">Email:</label>
         <input type="email" id="email" wire:model="email" >
         <div>@error('email') {{ $message }} @enderror</div>
         <br>
-        <label for="title">Birth:</label>
+        <label for="birth">Birth:</label>
         <input type="text" id="birth" wire:model="birth" >
         <div>@error('birth') {{ $message }} @enderror</div>
         <br>
-        <label for="title">Status:</label>
+        <label for="status">Status:</label>
 
 
-        <select wire:model="status">
+        <select wire:model="status" name="status">
             <option value="none">None</option>
             @foreach(\App\Enums\FamilyStatusEnum::forSelect() as $name)
                 <option value="{{$name}}">{{$name}}</option>
             @endforeach
         </select>
 
-
-        <div>Status @json($status)</div>
         <div>@error('status') {{ $message }} @enderror</div>
         <br>
         <label for="title">Phone:</label>
