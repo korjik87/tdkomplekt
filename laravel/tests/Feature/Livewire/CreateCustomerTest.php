433af38eb39c22e8b17504a20e87a607dfe338f9
Sergey <sprakapovich@buffergroup.com>
2024-02-09 00:04:11 +0300

    test fake

--- laravel/tests/Feature/Livewire/CreateCustomerTest.php
+++ laravel/tests/Feature/Livewire/CreateCustomerTest.php
@@ -38,7 +38,61 @@ class CreateCustomerTest extends TestCase
 
         Livewire::test(CreateCustomer::class)
             ->set('name', fake()->firstName())
+            ->set('surname', fake()->lastName())
+            ->set('patronymic', fake()->firstName())
+            ->set('phones', [fake()->phoneNumber()])
+            ->set('email', fake()->email())
+            ->set('birth', fake()->date())
+            ->set('about', fake()->text())
+            ->set('files', [fake()->image()])
+            ->set('checkbox', fake()->boolean)
             ->call('save')
             ->assertHasErrors();
+
+
+//        Livewire::test(CreateCustomer::class)
+//            ->set('name', fake()->firstName())
+//            ->set('surname', fake()->lastName())
+//            ->set('patronymic', fake()->firstName())
+//            ->set('phones', [fake()->phoneNumber()])
+//            ->set('email', fake()->email())
+//            ->set('birth', fake()->date())
+//            ->set('about', fake()->text())
+//            ->set('files', [fake()->image()])
+//            ->set('checkbox', true)
+//            ->call('save')
+//            ->assertHasNoErrors();
+
+
+        $d = Livewire::test(CreateCustomer::class)
+            ->set('name', fake()->firstName())
+            ->set('surname', fake()->lastName())
+            ->set('patronymic', fake()->firstName())
+            ->set('phones', [fake()->phoneNumber()])
+            ->set('email', fake()->email())
+            ->set('birth', fake()->date())
+            ->set('about', fake()->text())
+            ->set('files', [fake()->image()])
+            ->set('checkbox', true)
+            ->errors();
+
+        dump($d);
+
+        $d = Livewire::test(CreateCustomer::class)
+            ->set('name', fake()->firstName())
+            ->set('surname', fake()->lastName())
+            ->set('patronymic', fake()->firstName())
+            ->set('phones', [fake()->phoneNumber()])
+            ->set('email', fake()->email())
+            ->set('birth', fake()->date())
+            ->set('about', fake()->text())
+            ->set('files', [fake()->image()])
+            ->set('checkbox', true)
+            ->call('save')
+            ->errors();
+
+
+        dump($d);
+
     }
 }
